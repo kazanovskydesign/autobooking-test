8a8540c48c4160f1c64fa8f9754032ddac4d9738
Dmitry Kazanovsky <dimakazanovsky@gmail.com>
2019-09-15 00:46:10 +0300

    Updates

---
+++ precache-manifest.a1e9a83d2fd71f70cb513e2c7760a2e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d124c034e2244163446dce5ed8f8d9",
+    "url": "/autobooking-test/index.html"
+  },
+  {
+    "revision": "34a904b8d68a4e55407c",
+    "url": "/autobooking-test/static/css/main.553fff54.chunk.css"
+  },
+  {
+    "revision": "dd29e9d30ad46c8f0fea",
+    "url": "/autobooking-test/static/js/2.b50c6b88.chunk.js"
+  },
+  {
+    "revision": "34a904b8d68a4e55407c",
+    "url": "/autobooking-test/static/js/main.bf0bed04.chunk.js"
+  },
+  {
+    "revision": "78412d9abdd6e0830fa3",
+    "url": "/autobooking-test/static/js/runtime~main.20305214.js"
+  }
+]);

d5b1d5915584facfd986653ec578d999ae899489
Dmitry Kazanovsky <dimakazanovsky@gmail.com>
2019-09-15 00:41:31 +0300

    add build

---
+++ build/precache-manifest.2d99e97b0a518f67ab111513cf9816b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04665fecf82173004c8a139c892667a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34a904b8d68a4e55407c",
+    "url": "/static/css/main.553fff54.chunk.css"
+  },
+  {
+    "revision": "dd29e9d30ad46c8f0fea",
+    "url": "/static/js/2.b50c6b88.chunk.js"
+  },
+  {
+    "revision": "34a904b8d68a4e55407c",
+    "url": "/static/js/main.bf0bed04.chunk.js"
+  },
+  {
+    "revision": "2b9862e150f16a116103",
+    "url": "/static/js/runtime~main.6c78cb8b.js"
+  }
+]);
